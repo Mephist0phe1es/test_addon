e128ac3cd1e818c3bfb5f099c088939b8b7968f3
Mephist <sashamashina2000@gmail.com>
2021-02-10 19:51:55 +0200

    added comments

--- addonver2.js
+++ addonver2.js
@@ -1,12 +1,12 @@
 let position = 0;
-var BR = document.querySelector('.btnRight');
-var BL = document.querySelector('.btnLeft');
-var BC = document.querySelector('.btnCentr');
+var BR = document.querySelector('.btnRight');           //Button Right
+var BL = document.querySelector('.btnLeft');           //Button Left
+var BC = document.querySelector('.btnCentr');           //Button Centr
 
-BR.addEventListener('click', () => {
-    position = -600;
-    const track = document.querySelector('.slider-track');
-    track.style.left = position + 'px';
+BR.addEventListener('click', () => {                                    //создание ивентов, которые вызовут срабатывание "слайда"
+    position = -600;                                                    // установка позиции для слайдера
+    const track = document.querySelector('.slider-track');              //создание переменной для слайдера
+    track.style.left = position + 'px';                                 // установка позиции слайдера
 });
 
 BL.addEventListener('click', () => {
